52b0c2e136d37816c59d093a35a74da06ebcf935
shampun511 <143274750+shampun511@users.noreply.github.com>
2023-10-14 13:39:13 +0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,32 @@
+import discord, random, typing, discord, os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir("images"))
+    with open(f'images/{img_name}', 'rb') as f:
+        # не с форматом txt
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def memrust(ctx):
+    img_name = random.choice(os.listdir("rustmemes"))
+    with open(f'rustmemes/{img_name}', 'rb') as f:
+        # не с форматом txt
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+
+
+
+bot.run("MTE1MDA3MzY1Nzg3MjM1MTI0Mw.Guhf2p.ocd1xWApu4XtNxzNXD_pXnJOnrz08nx3qcAFAw")
